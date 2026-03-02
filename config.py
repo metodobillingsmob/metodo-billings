34e475621a314b7ef78aed5bb094e78b66ace7a9
metodobillingsmob <metodobillings.mob@gmail.com>
2026-03-02 15:34:39 -0300

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'uma-chave-muito-secreta-123'
@@ -6,12 +7,24 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # =========================
+    # SESSÃO / LOGIN
+    # =========================
+    REMEMBER_COOKIE_DURATION = timedelta(days=90)
+    SESSION_PERMANENT = True
+    PERMANENT_SESSION_LIFETIME = timedelta(days=90)
+
+    # =========================
     # SENDGRID
+    # =========================
     SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
     EMAIL_SENDER = os.environ.get("EMAIL_SENDER")
 
+    # =========================
     # TWILIO
+    # =========================
     TWILIO_SID = os.environ.get("TWILIO_SID")
     TWILIO_TOKEN = os.environ.get("TWILIO_TOKEN")
+
 
 
