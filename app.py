d7e8c6a7e1927500cd9b2c992205b38d5760d5a2
metodobillingsmob <metodobillings.mob@gmail.com>
2026-04-10 01:22:13 -0300

    Update app.py - tentativa resolução erro 502

--- app.py
+++ app.py
@@ -433,4 +433,7 @@ def manual():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
